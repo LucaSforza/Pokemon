5a13979bbdc53460ca1fa22ac9ea368218693add
RafMer00 <raffi.mercurio@gmail.com>
2025-10-20 14:36:47 +0200

    non funziona ancora

--- data_analyzer/lib.py
+++ data_analyzer/lib.py
@@ -145,8 +145,9 @@ def check_status_pokemon(cur: sqlite3.Cursor, team: pd.DataFrame, primary: bool,
     return pd.concat(results, ignore_index=True)
 
 def check_status_complete(cur: sqlite3.Cursor, team: pd.DataFrame, primary: bool,battle_id: int, _set: str) -> pd.DataFrame:
-    team_complete = get_teams_complete(cur, battle_id, _set)[0 if primary else 1]
-    team_complete = team_complete.drop(columns=["base_hp"]) # secondo me dovremmo moltiplicare dalla vita rimasta agli hp
+    team_complete = get_teams_complete(cur, battle_id, _set)[0 if primary else 1] 
+
+    # secondo me dovremmo moltiplicare dalla vita rimasta agli hp
     # TODO: moltiplicare base_hp con la percentuale rimasta
     id_battle = find_id_battle(cur, battle_id, _set)
     pokemon_status = check_status_pokemon(cur, team, primary,id_battle)
@@ -159,6 +160,12 @@ def check_status_complete(cur: sqlite3.Cursor, team: pd.DataFrame, primary: bool
         "hp_pct": 1, "boost_atk": 0, "boost_def": 0, "boost_spa": 0, "boost_spd": 0, "boost_spe": 0, "status": "nostatus", "pok_move": "nomove"})
     complete_pokemon_status['types'] = complete_pokemon_status['types'].apply(
         lambda x: x if isinstance(x, (list)) else ['notype'])
+
+    complete_pokemon_status["hp_final"] = complete_pokemon_status["base_hp"] * complete_pokemon_status["hp_pct"]
+    complete_pokemon_status = complete_pokemon_status.drop(columns=["hp_pct", "base_hp", "pok_move"])
+        
+    columns_to_reset = ["base_atk", "base_def", "base_spa", "base_spd", "base_spe", "boost_atk", "boost_def", "boost_spa", "boost_spd", "boost_spe"]
+    complete_pokemon_status.loc[complete_pokemon_status['hp_final'] == 0, columns_to_reset] = 0
 
     return complete_pokemon_status
 
@@ -203,4 +210,60 @@ def get_all_status(cur: sqlite3.Cursor) -> list[str]:
 # TODO: implement
 # Fai una query alla tabella input e dividili in due datafram X e Y
 def get_datapoints(cur: sqlite3.Cursor, _set: str) -> tuple[pd.DataFrame, pd.DataFrame]:
-    pass
+   pass 
+    
+def get_teams_features(cur: sqlite3.Cursor, battle_id: int, _set: str, all_status: pd.DataFrame) -> pd.DataFrame:
+    all_types = get_all_types(cur)
+    mlb = MultiLabelBinarizer(classes=all_types)
+    mlb.fit([all_types])
+
+    all_pokemon_status = get_all_status(cur)
+    ohe = OneHotEncoder(categories=[all_pokemon_status], sparse_output=False, handle_unknown='ignore')
+    
+    team1, team2 = get_teams(cur, battle_id, _set)
+    status1 = check_status_complete(cur, team1,True, battle_id, _set)
+    status2 = check_status_complete(cur, team2,False, battle_id, _set)              
+
+    status1 = status1.add_prefix('p1_')
+    status2 = status2.add_prefix('p2_')
+
+    status1 = status1.drop(columns=["p1_name"])
+    status2 = status2.drop(columns=["p2_name"])
+
+    p1_type_encoded = pd.DataFrame(
+        mlb.transform(status1['p1_types']),
+        columns=[f'p1_type_{t}' for t in mlb.classes_],
+        index=status1.index)
+
+    p2_type_encoded = pd.DataFrame(                    
+        mlb.transform(status2['p2_types']),
+        columns=[f'p2_type_{t}' for t in mlb.classes_],
+        index=status2.index)
+    
+    # attacco gli altri attributi, si riferiscono per ogni tabella e per ogni pokemon
+    status1 = pd.concat([status1.drop(columns=['p1_types']), p1_type_encoded], axis=1)
+    status2 = pd.concat([status2.drop(columns=['p2_types']), p2_type_encoded], axis=1)
+
+    # TODO: collassarli facendo la combinazione lineare delle due
+    p1_status_encoded = pd.DataFrame(
+        ohe.fit_transform(status1[['p1_status']]),
+        columns=[f'p1_status_{s}' for s in all_pokemon_status],
+        index=status1.index
+    )
+
+    p2_status_encoded = pd.DataFrame(
+        ohe.fit_transform(status2[['p2_status']]),
+        columns=[f'p2_status_{s}' for s in all_pokemon_status],
+        index=status2.index
+    )
+
+    print(p2_status_encoded)
+    
+    # TODO: collassarli facendo la combinazione lineare delle due
+    status1 = pd.concat([status1.drop(columns=['p1_status']), p1_status_encoded], axis=1)
+    status2 = pd.concat([status2.drop(columns=['p2_status']), p2_status_encoded], axis=1)
+
+    status = pd.concat([status1, status2], axis=1)
+    status_aggregated = pd.DataFrame(status.sum()).T
+    
+    all_status = pd.concat([all_status, status_aggregated], ignore_index=True)
